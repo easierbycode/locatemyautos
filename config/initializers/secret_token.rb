6885b1fa1501fa0fa00f790c9ff7d1319087d16e
Daniel Johnson <navseeker7@gmail.com>
2013-02-25 21:00:27 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Locatemyautos::Application.config.secret_token = '734954b81a8fe46ce4180cb259161937056267ad4e8606be84fb5f2c8bc2a4c73d35050b4dfc13739ce0e50d4b95a680bf0d903283e99b7723503b82c56fde4d'
