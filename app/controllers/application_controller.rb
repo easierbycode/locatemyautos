de6538371ce79a9f6b87fd5a362907d8f9a8c164
easierbycode <navseeker7@gmail.com>
2013-03-28 02:42:51 -0700

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  around_filter :scope_current_dealer_group
+  # around_filter :scope_current_dealer_group
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
@@ -10,14 +10,12 @@ class ApplicationController < ActionController::Base
 private
 
   def current_dealer_group
-    @current_dealer_group ||= DealerGroup.find_by_subdomain!(request.subdomain)
+    # @current_dealer_group ||= DealerGroup.find_by_subdomain!(request.subdomain)
   end
   helper_method :current_dealer_group
 
   def scope_current_dealer_group(&block)
-    if request.subdomain
-      current_dealer_group.scope_schema("public", &block)
-    end
+    # current_dealer_group.scope_schema("public", &block)
   end
 
 end
